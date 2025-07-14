b79404cfe58f42a245071671fd0d63aaefa0d4f1
Zaid <mohammedzaidmadari@gmail.com>
2025-07-14 11:48:28 +0530

    landing page UI changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,16 @@
-// import React from "react";
-import Navbar from "../../components/Navbar";
 import "./home.css";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="home-container">
       <div className="my_posts">
         <h1>My Posts :</h1>
       </div>
 
       <div className="content-container">
         <div className="title">
-          <h2>Enginuity</h2>
+          <h2>Enginuity -</h2>
+          <h3>Engineering Talent & Project Matching Platform</h3>
           <span className="date">Tue, Apr 1, 2025</span>
           <p className="content">
             Enginuity is a next-gen platform connecting engineers with jobs &
@@ -19,12 +18,13 @@ const Home = () => {
             talent. Whether you're a developer, mechanical engineer, or IoT
             expert, Enginuity helps match you with the perfect opportunity. We
             streamline the entire matching process, saving time for both talent
-            and teams...{" "}
+            and teams.{" "}
           </p>
         </div>
 
         <div className="title">
-          <h2>Real Time People Counting</h2>
+          <h2>Real Time People Counting -</h2>
+          <h3>Real Time Occupancy Insights</h3>
           <span className="date">Mon, Sep 16, 2024</span>
           <p className="content">
             A real-time people counting system powered by computer vision. It
@@ -33,24 +33,26 @@ const Home = () => {
             Python and OpenCV, the system is scalable for crowd monitoring,
             security, and occupancy management. Easily integrable with existing
             CCTV setups, it delivers actionable insights with minimal hardware
-            requirements...{" "}
+            requirements.{" "}
           </p>
         </div>
 
         <div className="title">
-          <h2>Text To Image generation</h2>
+          <h2>Text To Image generation -</h2>
+          <h3>Text Prompted Visuals</h3>
           <span className="date">Sun, Sep 8, 2024</span>
           <p className="content">
             Stable Diffusion Image Generation Generate high-quality images from
             text prompts with ease using the Stable Diffusion model. This
             project features a streamlined setup with Hugging Face’s Diffusers
             and Transformers libraries, enabling fast and flexible AI-powered
-            image creation and resizing...{" "}
+            image creation and resizing.{" "}
           </p>
         </div>
 
         <div className="title">
-          <h2>Exploring Text-to-Image Generation</h2>
+          <h2>Exploring Text-to-Image Generation -</h2>
+          <h3>A Comparative Study of GANs and Diffusion</h3>
           <span className="date">Sun, Sep 8, 2024</span>
           <p className="content">
             This research investigates various methods for text-to-image
@@ -61,7 +63,7 @@ const Home = () => {
             improve prompt understanding and image quality. After testing
             multiple methods, we selected the pretrained Stable Diffusion v1.4
             model with a U-Net backbone for its superior performance and
-            reliability...{" "}
+            reliability..{" "}
           </p>
         </div>
       </div>
